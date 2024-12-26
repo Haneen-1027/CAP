ed3ace5ce5b84a9b74db361640bd732a76616f55
OmarKH24 <omark.nasser24@gmail.com>
2024-12-26 13:27:54 +0200

    Initial commit: Set up React project with folder structure, global CSS, and installed libraries (jwtDecode, Axios, Joi, react-router)

---
+++ Frontend/client/src/App.jsx
@@ -0,0 +1,8 @@
+import React, { Suspense, useEffect, useState } from "react";
+import { jwtDecode } from "jwt-decode";
+
+function App() {
+  return <div className="light-theme">Here we go.</div>;
+}
+
+export default App;
