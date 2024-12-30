14cd4f44dffbe8958c9031c385e12753efafb8b3
mohammadjuryMENA <MENA1070@menadevs.io>
2024-12-30 21:11:35 +0200

    Adding gitignore and pushing basic commit for backend

---
+++ Backend/CapBackend/webApi/Program.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Mvc; 
+using Microsoft.EntityFrameworkCore;
+using webApi.Controllers;
+using webApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer(); // Required for Swagger
+builder.Services.AddSwaggerGen(); // Add Swagger support
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger(); // Enable Swagger middleware
+    app.UseSwaggerUI(); // Enable Swagger UI
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.UseAuthorization();
+app.UseCors("CorsPolicy");
+
+// Map controllers
+app.MapControllers();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
