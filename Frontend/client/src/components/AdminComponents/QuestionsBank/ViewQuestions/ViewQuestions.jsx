55bb794b18d76c88ec441656c1d477831e073528
OmarNasser08 <MENA1005@menadevs.io>
2025-01-30 16:38:03 +0200

    Frontend: Preview Questions for Admin page V1.1

--- Frontend/client/src/components/AdminComponents/QuestionsBank/ViewQuestions/ViewQuestions.jsx
+++ Frontend/client/src/components/AdminComponents/QuestionsBank/ViewQuestions/ViewQuestions.jsx
@@ -6,6 +6,7 @@ import {
 
 export default function ViewQuestions({ userDetailes, darkMode }) {
   const categories = [
+    { name: "All Categories", value: "all" },
     { name: "HTML", value: "html" },
     { name: "CSS", value: "css" },
     { name: "JavaScript", value: "js" },
@@ -14,7 +15,13 @@ export default function ViewQuestions({ userDetailes, darkMode }) {
     { name: "Angular", value: "angular" },
     { name: "React", value: "react" },
   ];
-  const [category, setCategory] = useState("");
+  const questionTypes = [
+    { name: "Multible Choice", value: "mc" },
+    { name: "Essay Question", value: "essay" },
+    { name: "Coding Question", value: "coding" },
+  ];
+  const [questionType, setQuestionType] = useState("mc");
+  const [category, setCategory] = useState("all");
   const countPerPageValues = [10, 15, 25, 50, 75, 100];
   const [countPerPage, setCounPerPage] = useState(25);
   const [pageNo, setPageNo] = useState(1);
@@ -26,6 +33,9 @@ export default function ViewQuestions({ userDetailes, darkMode }) {
   function handleCategory(e) {
     setCategory(e.target.value);
   }
+  function handleType(e) {
+    setQuestionType(e.target.value);
+  }
 
   ///////////////////////
   useEffect(() => {
@@ -34,40 +44,92 @@ export default function ViewQuestions({ userDetailes, darkMode }) {
   ///////////////
   return (
     <>
-      <div className="d-flex justify-content-between align-items-center p-4">
-        <div className="category">
-          <FilterableDropdown
-            darkMode={darkMode}
-            filterType={"Select Category:"}
-            items={categories}
-            handleFunction={handleCategory}
-          />
+      <div className="mx-3 mt-4">
+        <div className="row gap-4 gap-lg-0 my-1">
+          <div className="col-12 col-lg-4 d-flex flex-column flex-lg-row gap-2">
+            <div className="category">
+              <FilterableDropdown
+                darkMode={darkMode}
+                filterType={"Select Category:"}
+                items={categories}
+                handleFunction={handleCategory}
+              />
+            </div>
+            <div className="type">
+              <FilterableDropdown
+                darkMode={darkMode}
+                filterType={"Select Question Type:"}
+                items={questionTypes}
+                handleFunction={handleType}
+              />
+            </div>
+          </div>
+          <div className="col-12 col-lg-5">
+            <PaginationNav
+              darkMode={darkMode}
+              counts={questionsCount}
+              pageNo={pageNo}
+              setPageNo={setPageNo}
+              countPerPage={countPerPage}
+            />
+          </div>
+          <div className="count-per-page col-12 col-lg-3 d-flex flex-column flex-lg-row gap-2">
+            <label className="" style={{ fontSize: "0.95rem" }}>
+              Questions per Page:
+            </label>
+            <select
+              className="form-select"
+              aria-label="Default select example"
+              value={countPerPage}
+              onChange={handleCountPerPageMenu}
+            >
+              {countPerPageValues.map((value, index) => (
+                <option key={index} value={value}>
+                  {value}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+        <div className="position-relative mt-3 mb-5">
+          {" "}
+          <hr className="" />
         </div>
         <div>
-          <PaginationNav
-            darkMode={darkMode}
-            counts={questionsCount}
-            pageNo={pageNo}
-            setPageNo={setPageNo}
-            countPerPage={countPerPage}
-          />
-        </div>
-        <div className="count-per-page d-flex">
-          <label className="w-100" style={{ fontSize: "0.95rem" }}>
-            Questions per Page:
-          </label>
-          <select
-            className="form-select w-50"
-            aria-label="Default select example"
-            value={countPerPage}
-            onChange={handleCountPerPageMenu}
-          >
-            {countPerPageValues.map((value, index) => (
-              <option key={index} value={value}>
-                {value}
-              </option>
-            ))}
-          </select>
+          <div className="row d-flex align-items-center">
+            <div className="col-4 col-md-2 d-flex gap-4">
+              <div className="">1.</div>
+              <div className="">HTML</div>
+            </div>
+            <div className="col-8 col-md-5 text-truncate">
+              This is the Question that we want to render. it should be toooooo
+              long. This is the Question that we want to render. it should be
+              toooooo long.
+            </div>
+            <div className="col-6 col-md-3">
+              {questionType === "mc"
+                ? 1
+                  ? "True/False"
+                  : "Multiple Choice"
+                : ""}
+            </div>
+            <div className="col-6 col-md-2 d-flex justify-content-end gap-2">
+              <div
+                className={`btn view-button ${darkMode ? "text-light" : ""}`}
+              >
+                <i class="fa-regular fa-eye"></i>
+              </div>
+              <div
+                className={`btn delete-button ${darkMode ? "text-light" : ""}`}
+              >
+                <i className="fa-solid fa-trash" />
+              </div>
+            </div>
+          </div>
+          <div className="position-relative my-1">
+            {" "}
+            <hr className="" />
+          </div>
         </div>
       </div>
     </>
