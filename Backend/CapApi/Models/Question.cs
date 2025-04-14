96143b18ce581450d96fca4b93e2a3b348b4b237
mohammadjuryMENA <MENA1070@menadevs.io>
2025-04-14 11:07:45 +0300

    changed size of prompt in question model to 1000 char

--- Backend/CapApi/Models/Question.cs
+++ Backend/CapApi/Models/Question.cs
@@ -7,7 +7,7 @@ public class Question
     public int Id { get; init; }
     [MaxLength(100)] public string? Type { get; set; } // "mc", "coding", "essay"
     [MaxLength(100)] public string? Category { get; set; }
-    [MaxLength(100)] public string? Prompt { get; set; }
+    [MaxLength(1000)] public string? Prompt { get; set; }
     public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
     public McqQuestion? McqQuestion { get; set; }
