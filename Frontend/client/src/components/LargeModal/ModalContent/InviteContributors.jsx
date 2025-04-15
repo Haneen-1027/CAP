849af69a104368f83e5b3278d8b7474e20309d83
OmarNasser08 <MENA1005@menadevs.io>
2025-04-16 00:57:18 +0300

    Frontend: Invite contributors by email 'Functionality'

--- Frontend/client/src/components/LargeModal/ModalContent/InviteContributors.jsx
+++ Frontend/client/src/components/LargeModal/ModalContent/InviteContributors.jsx
@@ -1,4 +1,64 @@
+import { useEffect, useState } from "react";
+
 export default function InviteContributors({ darkMode }) {
+  const [invite, setInvite] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+  });
+  const [invites, setInvites] = useState([]);
+
+  const [validationMessage, setValidationMessage] = useState("");
+
+  // Add New Invitation
+  function addNewInvite(e) {
+    const isEmailExists = invites.some((i) =>
+      Object.values(i).includes(invite.email)
+    );
+    if (isEmailExists) {
+      setValidationMessage("This email is already in the list!");
+    } else {
+      setInvites((prevInvites) => [...prevInvites, invite]);
+      setInvite({
+        first_name: "",
+        last_name: "",
+        email: "",
+      });
+    }
+  }
+
+  // Remove a record from the array based on (email)
+  function removeRecord(inv) {
+    const updatedList = invites.filter((invite) => invite.email !== inv.email);
+    setInvites(updatedList);
+  }
+
+  // Render Invites in the table
+  function renderInvites() {
+    return invites.map((inv, index) => (
+      <tr key={index} className="">
+        <td>{index < 10 ? "0" + (index + 1) : index + 1}</td>
+        <td>{inv.first_name + " " + inv.last_name}</td>
+        <td>{inv.email}</td>
+        <td>
+          <button
+            className="btn btn-danger btn-sm"
+            onClick={() => removeRecord(inv)}
+          >
+            X
+          </button>
+        </td>
+      </tr>
+    ));
+  }
+  ///////////
+
+  useEffect(() => {
+    console.log("Invites List: ", invites);
+  }, [invites]);
+  useEffect(() => {
+    console.log("new invite: ", invite);
+  }, [invite]);
   return (
     <>
       <div className={`card ${darkMode ? " spic-dark-mode" : ""}`}>
@@ -18,19 +78,71 @@ export default function InviteContributors({ darkMode }) {
             darkMode ? " spic-dark-mode" : ""
           }`}
         >
-          <form className="form-floating col-12 col-md-11">
+          <form className="form-floating col-12">
+            <input
+              type="text"
+              className="form-control"
+              id="floatingInputValue"
+              name="first_name"
+              placeholder="First Name"
+              value={invite.first_name}
+              onChange={(e) =>
+                setInvite((prevInvite) => ({
+                  ...prevInvite,
+                  first_name: e.target.value,
+                }))
+              }
+            />
+            <label htmlFor="floatingInputValue" className="mx-4">
+              First Name:
+            </label>
+          </form>
+          <form className="form-floating col-12 my-2">
+            <input
+              type="text"
+              className="form-control"
+              id="floatingInputValue"
+              name="last_name"
+              placeholder="Last Name"
+              value={invite.last_name}
+              onChange={(e) =>
+                setInvite((prevInvite) => ({
+                  ...prevInvite,
+                  last_name: e.target.value,
+                }))
+              }
+            />
+            <label htmlFor="floatingInputValue" className="mx-4">
+              Last Name:
+            </label>
+          </form>
+          <form className="form-floating col-12 mb-2">
             <input
               type="email"
               className="form-control"
               id="floatingInputValue"
+              name="email"
               placeholder="name@example.com"
+              value={invite.email}
+              onChange={(e) =>
+                setInvite((prevInvite) => ({
+                  ...prevInvite,
+                  email: e.target.value,
+                }))
+              }
             />
             <label htmlFor="floatingInputValue" className="mx-4">
               Add Email - "name@example.com"
             </label>
           </form>
-          <div className="col-12 col-md-1 my-2 m-md-0 d-flex justify-content-center">
-            <button className="btn btn-success">Add</button>
+          <div className="col-12 my-2 m-md-0 d-flex justify-content-center">
+            <button
+              className="btn btn-success"
+              style={{ width: "10rem" }}
+              onClick={(e) => addNewInvite(e)}
+            >
+              Add
+            </button>
           </div>
         </div>
         {/** Handle emails */}
@@ -43,20 +155,18 @@ export default function InviteContributors({ darkMode }) {
             <thead className={darkMode ? "spic-dark-mode" : "table-light"}>
               <tr>
                 <th>#</th>
+                <th>Name:</th>
                 <th>Email</th>
                 <th>Actions</th>
               </tr>
             </thead>
-            <tbody className="table-border-bottom-0">
-              <tr className="">
-                <td>01</td>
-                <td>test@example.com</td>
-                <td>
-                  <button className="btn btn-danger btn-sm">X</button>
-                </td>
-              </tr>
-            </tbody>
+            <tbody className="table-border-bottom-0">{renderInvites()}</tbody>
           </table>
+          <div className="my-4">
+            <button className="btn btn-success" style={{ width: "10rem" }}>
+              Send Invites
+            </button>
+          </div>
         </div>
       </div>
     </>
