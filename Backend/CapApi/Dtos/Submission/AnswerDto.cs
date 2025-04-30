fcb273ec97b43c5d1be7529c8393d3ebedc4f27e
Eneirat <mena1051@menadevs.io>
2025-04-30 14:21:10 +0300

    Add submission API

---
+++ Backend/CapApi/Dtos/Submission/AnswerDto.cs
@@ -0,0 +1,8 @@
+namespace CapApi.Dtos.Submission;
+
+public class AnswerDto
+{
+    public int question_id { get; set; }
+    public string contributor_answer { get; set; }
+    public string question_type { get; set; }
+}
