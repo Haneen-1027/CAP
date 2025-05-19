b287aa8be341a2b841fc8d5a26e267ee9707561a
mohammadjuryMENA <MENA1070@menadevs.io>
2025-05-19 12:54:38 +0300

    submissionDto edits.

--- Backend/CapApi/Dtos/Submission/SubmissionDto.cs
+++ Backend/CapApi/Dtos/Submission/SubmissionDto.cs
@@ -3,12 +3,13 @@ namespace CapApi.Dtos.Submission;
 public class SubmissionDto
 {
     public int SubmissionId { get; set; }
-    public int QuestionId { get; set; }
-    public string QuestionText { get; set; }
-    public string QuestionType { get; set; }
-    public string Answer { get; set; }
-    public int Mark { get; set; }
-    public int MaxMark { get; set; }
+    public int? QuestionId { get; set; }
+    public string? QuestionText { get; set; }
+    public string? QuestionType { get; set; }
+    public string? Answer { get; set; }
+    public int? Mark { get; set; }
+    public int? MaxMark { get; set; }
+    public List<string>? PresentedOptions { get; set; }
     public DateTime SubmittedAt { get; set; }
-    public List<string> WrongOptions { get; set; }
+    public List<string>? WrongOptions { get; set; }
 }
