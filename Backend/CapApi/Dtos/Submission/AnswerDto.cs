92b7cd0d917800016b7b9b02a6eef566e44a6a94
Haneen-1027 <MENA1027@menadevs.io>
2025-05-22 01:46:49 +0300

    Assessment submission

--- Backend/CapApi/Dtos/Submission/AnswerDto.cs
+++ Backend/CapApi/Dtos/Submission/AnswerDto.cs
@@ -1,3 +1,5 @@
+using CapApi.Dtos.Question;
+
 namespace CapApi.Dtos.Submission;
 
 public class AnswerDto
@@ -9,4 +11,10 @@ public class AnswerDto
     // For coding questions
     public int? test_pass { get; set; }
     public int? total_test_case { get; set; }
+
+    public string prompt { get; set; }
+    //public QuestionDetailsDto? question_details { get; set; }
+    public object question_details { get; set; }
+    public int new_mark { get; set; }
+    public int total_mark { get; set; }
 }
